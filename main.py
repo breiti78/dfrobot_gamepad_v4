5fc7e0bd80ec427a1e80f874d846b326615b1445
Candelario Eguia <candelario.eguia@laverne.edu>
2021-08-31 22:00:11 -0700

    moved code to functions and moved call into function

--- main.py
+++ main.py
@@ -1,96 +1,194 @@
-leftButton2 = 0
-rightButton2 = 0
-backwardButton2 = 0
-radio.set_group(1)
-forwardButton = 1
-backwardButton = 1
-rightButton = 1
-leftButton = 1
-
-def on_forever():
-    global forwardButton, backwardButton2, rightButton2, leftButton2
-    forwardButton = pins.digital_read_pin(DigitalPin.P15)
-    backwardButton = pins.digital_read_pin(DigitalPin.P13)
-    rightButton = pins.digital_read_pin(DigitalPin.P14)
-    leftButton = pins.digital_read_pin(DigitalPin.P16)
+def showStick():
+    basic.show_leds("""
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        . . # . .
+    """)
+def incramental():
+    global forwardButton, backwardButton, rightButton, leftButton
+    setVarsToPins()
     if forwardButton == 0:
-        radio.send_string("FA")
+        radio.send_string("slowForward")
+        basic.show_leds("""
+            . . # . .
+            . # . # .
+            # . . . #
+            . . . . .
+            . . . . .
+        """)
+        basic.pause(100)
         forwardButton = 1
+    elif backwardButton == 0:
+        radio.send_string("slowBackward")
+        backwardButton = 1
         basic.show_leds("""
+            . . . . .
+            . . . . .
+            # . . . #
+            . # . # .
             . . # . .
-                        . # # # .
-                        # . # . #
-                        . . # . .
-                        . . # . .
         """)
         basic.pause(100)
-    elif pins.digital_read_pin(DigitalPin.P13) == 0:
-        radio.send_string("BA")
+    elif rightButton == 0:
+        radio.send_string("oneDegreeRight")
+        rightButton = 1
         basic.show_leds("""
             . . # . .
-                        . . # . .
-                        # . # . #
-                        . # # # .
-                        . . # . .
+            . . . # .
+            . . . . #
+            . . . # .
+            . . # . .
         """)
         basic.pause(100)
-    elif pins.digital_read_pin(DigitalPin.P14) == 0:
-        radio.send_string("RA")
+    elif leftButton == 0:
+        radio.send_string("oneDegreeLeft")
+        leftButton = 1
         basic.show_leds("""
             . . # . .
-                        . . . # .
-                        # # # # #
-                        . . . # .
-                        . . # . .
-        """)
-        basic.pause(100)
-    elif pins.digital_read_pin(DigitalPin.P16) == 0:
-        radio.send_string("LA")
-        basic.show_leds("""
+            . # . . .
+            # . . . .
+            . # . . .
             . . # . .
-                        . # . . .
-                        # # # # #
-                        . # . . .
-                        . . # . .
         """)
         basic.pause(100)
-    elif pins.analog_read_pin(AnalogPin.P2) > 550 and (pins.analog_read_pin(AnalogPin.P1) > 400 and pins.analog_read_pin(AnalogPin.P1) < 600):
+    else:
+        sendStop()
+def stickCheck():
+    if pins.analog_read_pin(AnalogPin.P2) > 550 and (pins.analog_read_pin(AnalogPin.P1) > 400 and pins.analog_read_pin(AnalogPin.P1) < 600):
         radio.send_value("F", pins.analog_read_pin(AnalogPin.P2))
         basic.show_leds("""
             . . # . .
-                        . # . # .
-                        # . . . #
-                        . . . . .
-                        . . . . .
+            . # . # .
+            # . # . #
+            . . . . .
+            . . # . .
         """)
     elif pins.analog_read_pin(AnalogPin.P2) < 450 and (pins.analog_read_pin(AnalogPin.P1) > 400 and pins.analog_read_pin(AnalogPin.P1) < 600):
         radio.send_value("B", pins.analog_read_pin(AnalogPin.P2))
         basic.show_leds("""
+            . . # . .
             . . . . .
-                        . . . . .
-                        # . . . #
-                        . # . # .
-                        . . # . .
+            # . # . #
+            . # . # .
+            . . # . .
         """)
     elif pins.analog_read_pin(AnalogPin.P1) < 450 and (pins.analog_read_pin(AnalogPin.P2) > 400 and pins.analog_read_pin(AnalogPin.P2) < 600):
         radio.send_value("L", pins.analog_read_pin(AnalogPin.P1))
         basic.show_leds("""
             . . # . .
-                        . # . . .
-                        # . . . .
-                        . # . . .
-                        . . # . .
+            . # . . .
+            # . # . #
+            . # . . .
+            . . # . .
         """)
     elif pins.analog_read_pin(AnalogPin.P1) > 550 and (pins.analog_read_pin(AnalogPin.P2) > 400 and pins.analog_read_pin(AnalogPin.P2) < 600):
         radio.send_value("R", pins.analog_read_pin(AnalogPin.P1))
         basic.show_leds("""
             . . # . .
-                        . . . # .
-                        . . . . #
-                        . . . # .
-                        . . # . .
+            . . . # .
+            # . # . #
+            . . . # .
+            . . # . .
         """)
     else:
-        radio.send_string("S")
-        basic.show_icon(IconNames.NO)
+        sendStop()
+def sendStop():
+    radio.send_string("S")
+def setVarsToPins():
+    global forwardButton, backwardButton, rightButton, leftButton
+    forwardButton = pins.digital_read_pin(DigitalPin.P15)
+    backwardButton = pins.digital_read_pin(DigitalPin.P13)
+    rightButton = pins.digital_read_pin(DigitalPin.P14)
+    leftButton = pins.digital_read_pin(DigitalPin.P16)
+def showButtons():
+    basic.show_leds("""
+        . # # # .
+        # . # . #
+        # # . # #
+        # . # . #
+        . # # # .
+    """)
+def setPins():
+    pins.set_pull(DigitalPin.P15, PinPullMode.PULL_UP)
+    pins.set_pull(DigitalPin.P13, PinPullMode.PULL_UP)
+    pins.set_pull(DigitalPin.P14, PinPullMode.PULL_UP)
+    pins.set_pull(DigitalPin.P16, PinPullMode.PULL_UP)
+def loop():
+    global stickControl
+    if input.button_is_pressed(Button.A):
+        if stickControl:
+            stickControl = False
+        else:
+            stickControl = True
+    if stickControl:
+        showStick()
+        stickCheck()
+    elif input.button_is_pressed(Button.B):
+        showButtons()
+        incramental()
+    else:
+        showButtons()
+        buttonCheck()
+def buttonCheck():
+    global forwardButton, backwardButton, rightButton, leftButton
+    setVarsToPins()
+    if forwardButton == 0:
+        radio.send_string("FA")
+        forwardButton = 1
+        basic.show_leds("""
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+        """)
+        basic.pause(100)
+    elif backwardButton == 0:
+        radio.send_string("BA")
+        backwardButton = 1
+        basic.show_leds("""
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+        """)
+        basic.pause(100)
+    elif rightButton == 0:
+        radio.send_string("RA")
+        rightButton = 1
+        basic.show_leds("""
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+        """)
+        basic.pause(100)
+    elif leftButton == 0:
+        radio.send_string("LA")
+        leftButton = 1
+        basic.show_leds("""
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+        """)
+        basic.pause(100)
+    else:
+        sendStop()
+leftButton = 0
+rightButton = 0
+backwardButton = 0
+forwardButton = 0
+stickControl = False
+radio.set_group(1)
+stickControl = False
+setPins()
+
+def on_forever():
+    loop()
 basic.forever(on_forever)
--- main.ts
+++ main.ts
@@ -1,15 +1,27 @@
-function incramental () {
+function showStick() {
+    basic.showLeds(`
+        . # # # .
+        # # # # #
+        . # # # .
+        . . # . .
+        . . # . .
+    `)
+}
+
+function incramental() {
+    
+    setVarsToPins()
     if (forwardButton == 0) {
         radio.sendString("slowForward")
-        forwardButton = 1
         basic.showLeds(`
             . . # . .
             . # . # .
             # . . . #
             . . . . .
             . . . . .
-            `)
+        `)
         basic.pause(100)
+        forwardButton = 1
     } else if (backwardButton == 0) {
         radio.sendString("slowBackward")
         backwardButton = 1
@@ -19,7 +31,7 @@ function incramental () {
             # . . . #
             . # . # .
             . . # . .
-            `)
+        `)
         basic.pause(100)
     } else if (rightButton == 0) {
         radio.sendString("oneDegreeRight")
@@ -30,7 +42,7 @@ function incramental () {
             . . . . #
             . . . # .
             . . # . .
-            `)
+        `)
         basic.pause(100)
     } else if (leftButton == 0) {
         radio.sendString("oneDegreeLeft")
@@ -41,13 +53,15 @@ function incramental () {
             # . . . .
             . # . . .
             . . # . .
-            `)
+        `)
         basic.pause(100)
     } else {
         sendStop()
     }
+    
 }
-function stickCheck () {
+
+function stickCheck() {
     if (pins.analogReadPin(AnalogPin.P2) > 550 && (pins.analogReadPin(AnalogPin.P1) > 400 && pins.analogReadPin(AnalogPin.P1) < 600)) {
         radio.sendValue("F", pins.analogReadPin(AnalogPin.P2))
         basic.showLeds(`
@@ -56,7 +70,7 @@ function stickCheck () {
             # . # . #
             . . . . .
             . . # . .
-            `)
+        `)
     } else if (pins.analogReadPin(AnalogPin.P2) < 450 && (pins.analogReadPin(AnalogPin.P1) > 400 && pins.analogReadPin(AnalogPin.P1) < 600)) {
         radio.sendValue("B", pins.analogReadPin(AnalogPin.P2))
         basic.showLeds(`
@@ -65,7 +79,7 @@ function stickCheck () {
             # . # . #
             . # . # .
             . . # . .
-            `)
+        `)
     } else if (pins.analogReadPin(AnalogPin.P1) < 450 && (pins.analogReadPin(AnalogPin.P2) > 400 && pins.analogReadPin(AnalogPin.P2) < 600)) {
         radio.sendValue("L", pins.analogReadPin(AnalogPin.P1))
         basic.showLeds(`
@@ -74,7 +88,7 @@ function stickCheck () {
             # . # . #
             . # . . .
             . . # . .
-            `)
+        `)
     } else if (pins.analogReadPin(AnalogPin.P1) > 550 && (pins.analogReadPin(AnalogPin.P2) > 400 && pins.analogReadPin(AnalogPin.P2) < 600)) {
         radio.sendValue("R", pins.analogReadPin(AnalogPin.P1))
         basic.showLeds(`
@@ -83,27 +97,69 @@ function stickCheck () {
             # . # . #
             . . . # .
             . . # . .
-            `)
+        `)
     } else {
         sendStop()
     }
+    
 }
-function sendStop () {
+
+function sendStop() {
     radio.sendString("S")
 }
-function setVarsToPins () {
+
+function setVarsToPins() {
+    
     forwardButton = pins.digitalReadPin(DigitalPin.P15)
     backwardButton = pins.digitalReadPin(DigitalPin.P13)
     rightButton = pins.digitalReadPin(DigitalPin.P14)
     leftButton = pins.digitalReadPin(DigitalPin.P16)
 }
-function setPins () {
+
+function showButtons() {
+    basic.showLeds(`
+        . # # # .
+        # . # . #
+        # # . # #
+        # . # . #
+        . # # # .
+    `)
+}
+
+function setPins() {
+    pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
     pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
     pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
-    pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
     pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
 }
-function buttonCheck () {
+
+function loop() {
+    
+    if (input.buttonIsPressed(Button.A)) {
+        if (stickControl) {
+            stickControl = false
+        } else {
+            stickControl = true
+        }
+        
+    }
+    
+    if (stickControl) {
+        showStick()
+        stickCheck()
+    } else if (input.buttonIsPressed(Button.B)) {
+        showButtons()
+        incramental()
+    } else {
+        showButtons()
+        buttonCheck()
+    }
+    
+}
+
+function buttonCheck() {
+    
+    setVarsToPins()
     if (forwardButton == 0) {
         radio.sendString("FA")
         forwardButton = 1
@@ -113,7 +169,7 @@ function buttonCheck () {
             # . # . #
             . . # . .
             . . # . .
-            `)
+        `)
         basic.pause(100)
     } else if (backwardButton == 0) {
         radio.sendString("BA")
@@ -124,7 +180,7 @@ function buttonCheck () {
             # . # . #
             . # # # .
             . . # . .
-            `)
+        `)
         basic.pause(100)
     } else if (rightButton == 0) {
         radio.sendString("RA")
@@ -135,7 +191,7 @@ function buttonCheck () {
             # # # # #
             . . . # .
             . . # . .
-            `)
+        `)
         basic.pause(100)
     } else if (leftButton == 0) {
         radio.sendString("LA")
@@ -146,65 +202,22 @@ function buttonCheck () {
             # # # # #
             . # . . .
             . . # . .
-            `)
+        `)
         basic.pause(100)
     } else {
         sendStop()
     }
+    
 }
+
 let leftButton = 0
 let rightButton = 0
 let backwardButton = 0
 let forwardButton = 0
-radio.setGroup(1)
-forwardButton = 1
-backwardButton = 1
-rightButton = 1
-leftButton = 1
 let stickControl = false
+radio.setGroup(1)
+stickControl = false
 setPins()
-basic.forever(function () {
-    setVarsToPins()
-    if (input.buttonIsPressed(Button.A)) {
-        if (stickControl) {
-            stickControl = false
-            basic.showLeds(`
-                . # # # .
-                # . # . #
-                # # . # #
-                # . # . #
-                . # # # .
-                `)
-        } else {
-            stickControl = true
-            basic.showLeds(`
-                . # # # .
-                # # # # #
-                . # # # .
-                . . # . .
-                . . # . .
-                `)
-        }
-    }
-    if (stickControl) {
-        stickCheck()
-        basic.showLeds(`
-            . # # # .
-            # # # # #
-            . # # # .
-            . . # . .
-            . . # . .
-            `)
-    } else if (input.buttonIsPressed(Button.B)) {
-        incramental()
-    } else {
-        buttonCheck()
-        basic.showLeds(`
-            . # # # .
-            # . # . #
-            # # . # #
-            # . # . #
-            . # # # .
-            `)
-    }
+basic.forever(function on_forever() {
+    loop()
 })
