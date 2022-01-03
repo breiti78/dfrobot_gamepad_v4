ec3044079b27d04c2bc712218b06d60f45c8b3f5
breiti78 <remo@breitis.ch>
2022-01-03 13:21:06 +0100

    Just send keys

--- main.ts
+++ main.ts
@@ -7,60 +7,44 @@ function showStick () {
         . . # . .
         `)
 }
-function stickCheck () {
-    if (pins.analogReadPin(AnalogPin.P2) > 550 && (pins.analogReadPin(AnalogPin.P1) > 400 && pins.analogReadPin(AnalogPin.P1) < 600)) {
-        radio.sendValue("forward", pins.analogReadPin(AnalogPin.P2))
-        basic.showLeds(`
-            . . # . .
-            . # . # .
-            # . # . #
-            . . . . .
-            . . # . .
-            `)
-    } else if (pins.analogReadPin(AnalogPin.P2) < 450 && (pins.analogReadPin(AnalogPin.P1) > 400 && pins.analogReadPin(AnalogPin.P1) < 600)) {
-        radio.sendValue("backward", pins.analogReadPin(AnalogPin.P2))
-        basic.showLeds(`
-            . . # . .
-            . . . . .
-            # . # . #
-            . # . # .
-            . . # . .
-            `)
-    } else if (pins.analogReadPin(AnalogPin.P1) < 450 && (pins.analogReadPin(AnalogPin.P2) > 400 && pins.analogReadPin(AnalogPin.P2) < 600)) {
-        radio.sendValue("left", pins.analogReadPin(AnalogPin.P1))
-        basic.showLeds(`
-            . . # . .
-            . # . . .
-            # . # . #
-            . # . . .
-            . . # . .
-            `)
-    } else if (pins.analogReadPin(AnalogPin.P1) > 550 && (pins.analogReadPin(AnalogPin.P2) > 400 && pins.analogReadPin(AnalogPin.P2) < 600)) {
-        radio.sendValue("right", pins.analogReadPin(AnalogPin.P1))
-        basic.showLeds(`
-            . . # . .
-            . . . # .
-            # . # . #
-            . . . # .
-            . . # . .
-            `)
+function readPins () {
+    x = pins.analogReadPin(AnalogPin.P1) - 512
+    y = pins.analogReadPin(AnalogPin.P2) - 512
+    if (pins.digitalReadPin(DigitalPin.P15) == 0) {
+        radio.sendString("P15")
+        last_button = "P15"
+        basic.pause(50)
+    } else if (pins.digitalReadPin(DigitalPin.P13) == 0) {
+        radio.sendString("P13")
+        last_button = "P13"
+        basic.pause(50)
+    } else if (pins.digitalReadPin(DigitalPin.P14) == 0) {
+        radio.sendString("P14")
+        last_button = "P14"
+        basic.pause(50)
+    } else if (pins.digitalReadPin(DigitalPin.P16) == 0) {
+        radio.sendString("P16")
+        last_button = "P16"
+        basic.pause(50)
+    } else if (x > 10 || x < -10) {
+        radio.sendValue("x", x)
+    } else if (y > 10 || y < -10) {
+        radio.sendValue("y", y)
+    } else if (pins.digitalReadPin(DigitalPin.P8) == 0) {
+        radio.sendString("P8")
+        last_button = "P8"
+        basic.pause(50)
     } else {
         sendStop()
     }
 }
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("A")
+    last_button = "A"
+    basic.pause(50)
+})
 function sendStop () {
     radio.sendString("S")
-}
-function setVarsToPins () {
-    if (pins.digitalReadPin(DigitalPin.P15) == 0) {
-        forwardButton = true
-    } else if (pins.digitalReadPin(DigitalPin.P13) == 0) {
-        backwardButton = true
-    } else if (pins.digitalReadPin(DigitalPin.P14) == 0) {
-        rightButton = true
-    } else if (pins.digitalReadPin(DigitalPin.P16) == 0) {
-        leftButton = true
-    }
 }
 function showButtons () {
     basic.showLeds(`
@@ -71,57 +55,42 @@ function showButtons () {
         . # # # .
         `)
 }
+input.onButtonPressed(Button.AB, function () {
+    radio.sendString("A+B")
+    last_button = "A+B"
+    basic.pause(50)
+})
 function setPins () {
     pins.setPull(DigitalPin.P15, PinPullMode.PullUp)
     pins.setPull(DigitalPin.P13, PinPullMode.PullUp)
     pins.setPull(DigitalPin.P14, PinPullMode.PullUp)
     pins.setPull(DigitalPin.P16, PinPullMode.PullUp)
+    pins.setPull(DigitalPin.P8, PinPullMode.PullUp)
 }
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("B")
+    last_button = "B"
+    basic.pause(50)
+})
 function buttonCheck () {
-    setVarsToPins()
-    if (forwardButton) {
-        radio.sendString("310")
+    readPins()
+    if (last_button == "P15") {
         images.arrowImage(ArrowNames.North).showImage(0)
-        basic.pause(100)
-    } else if (backwardButton) {
-        radio.sendString("410")
+    } else if (last_button == "P13") {
         images.arrowImage(ArrowNames.South).showImage(0)
-        basic.pause(100)
-    } else if (rightButton) {
-        radio.sendString("210")
+    } else if (last_button == "P14") {
         images.arrowImage(ArrowNames.East).showImage(0)
-        basic.pause(100)
-    } else if (leftButton) {
-        let LEFT = 0
-        radio.sendString("" + LEFT + "10")
+    } else if (last_button == "P16") {
         images.arrowImage(ArrowNames.West).showImage(0)
-        basic.pause(100)
     } else {
-        sendStop()
+    	
     }
 }
-let stickControl = false
-let leftButton = false
-let rightButton = false
-let backwardButton = false
-let forwardButton = false
+let last_button = ""
+let y = 0
+let x = 0
 radio.setGroup(1)
 setPins()
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.AB)) {
-        if (stickControl) {
-            stickControl = false
-        } else {
-            stickControl = true
-        }
-    }
-    if (stickControl) {
-        showStick()
-        stickCheck()
-    } else if (input.buttonIsPressed(Button.B)) {
-        showButtons()
-    } else {
-        showButtons()
-        buttonCheck()
-    }
+    readPins()
 })
